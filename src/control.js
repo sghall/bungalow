feffe5868369b4947e1c8a6ebd2688d5275a56f7
Steven Hall <hall.steven.g@gmail.com>
2015-05-09 01:06:37 -0700

    create control module

---
+++ src/control.js
@@ -0,0 +1,76 @@
+import THREE from 'THREE';
+import { rotater } from './rotate';
+import { zoom, mousewheel } from './zoom';
+import { getMouseOnCircle } from './mouse';
+
+var EPS = 0.000001;
+var lastPosition = new THREE.Vector3();
+
+var prev = new THREE.Vector2();
+var curr = new THREE.Vector2();
+
+var target = new THREE.Vector3();
+var source = new THREE.Vector3();
+
+export function createControl(camera) {
+  let rotate = rotater(camera);
+
+  return function() {
+
+    let quaternion = rotate(curr, prev);
+    let zoomFactor = zoom();
+
+    if (quaternion) {
+      source
+        .copy(camera.position).sub(target)
+        .applyQuaternion(quaternion)
+        .multiplyScalar(zoomFactor);
+
+      camera.up
+        .applyQuaternion(quaternion);
+
+    } else {
+      source
+        .copy(camera.position).sub(target)
+        .multiplyScalar(zoomFactor);
+    }
+
+    camera.position.addVectors(target, source);
+    camera.lookAt(target);
+
+    if (lastPosition.distanceToSquared(camera.position) > EPS) {
+      lastPosition.copy(camera.position);
+    }
+  };
+}
+
+function mousedown(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  curr.copy(getMouseOnCircle(event.pageX, event.pageY));
+  prev.copy(curr);
+
+  document.addEventListener('mousemove', mousemove, false);
+  document.addEventListener('mouseup', mouseup, false);
+}
+
+function mousemove(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  prev.copy(curr);
+  curr.copy(getMouseOnCircle(event.pageX, event.pageY));
+}
+
+function mouseup(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  document.removeEventListener('mousemove', mousemove);
+  document.removeEventListener('mouseup', mouseup);
+}
+
+document.addEventListener('mousedown', mousedown, false);
+document.addEventListener('mousewheel', mousewheel, false);
+document.addEventListener('DOMMouseScroll', mousewheel, false); // firefox
+
