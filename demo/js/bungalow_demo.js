04407605f5f6be3e0450c9fe88913bf52ef4f8c0
Steven Hall <hall.steven.g@gmail.com>
2015-05-09 23:07:39 -0700

    update demo to use classes

--- demo/js/bungalow_demo.js
+++ demo/js/bungalow_demo.js
@@ -1,16 +1,16 @@
 import THREE from 'THREE';
 import { camera, scene, renderer } from './scene';
-import { createControl } from '../../src/control';
+import { Control } from '../../src/control';
 
 var geometry = new THREE.BoxGeometry(400, 400, 400);
 var material = new THREE.MeshPhongMaterial({color: 'tomato'});
 var mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-var moveCamera = createControl(camera);
+var control = new Control(camera, renderer.domElement);
 
 function animate() {
-  moveCamera();
+  control.update();
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
